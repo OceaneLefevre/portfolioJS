3947e1aec63f05003df67f853425f9d4bb64fe84
OceaneLefevre <113765001+OceaneLefevre@users.noreply.github.com>
2022-12-19 00:00:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const button = document.querySelector("#nightMode");
+
+button.addEventListener("click", function() {
+  body.classList.toggle("modeNuit");
+})
+
+
+
+button.addEventListener("click", function() {
+    for (let i = 0; i < 6; i++) {
+    paragraphs[i].classList.toggle("animateText");
+  }  
+  })
+
